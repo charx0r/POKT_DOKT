5d143389ed2467325ae34cea9b6936a5aa5a9461
cventastic <sven@sven-holter.de>
2021-12-14 19:24:47 +0100

    changes for busybox tar

--- bootstrap_skript/pokt_mainnet.sh
+++ bootstrap_skript/pokt_mainnet.sh
@@ -3,6 +3,5 @@ if [ ! -f /home/app/.pocket/setupdone ]
 then
   mkdir -p /home/app/.pocket/data
   #chown -R 1005:1001 /home/app/
-  wget -qO- https://link.us1.storjshare.io/raw/jwb73f5wya2no5fcogrlpmmps7fq/pocket-public-blockchains/pocket-network-data-1214-rc-0.6.3.6.tar | tar -zxvf - /home/app/.pocket/data/
-  touch /home/app/.pocket/setupdone
+  wget -q -O - https://link.us1.storjshare.io/raw/jwb73f5wya2no5fcogrlpmmps7fq/pocket-public-blockchains/pocket-network-data-1214-rc-0.6.3.6.tar | tar -xv -C /home/app/.pocket/data/  touch /home/app/.pocket/setupdone
 fi
