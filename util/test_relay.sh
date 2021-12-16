dd334b73bcceac2689e9b590a6a26e6c2c3466d7
cventastic <sven@sven-holter.de>
2021-12-16 12:56:42 +0100

    fix testscript

--- util/test_relay.sh
+++ util/test_relay.sh
@@ -9,7 +9,6 @@ chain_arr[ropsten]="0023"
 chain_arr[mainnet]="0021"
 
 for i in ${!chain_arr[@]}; do
-  echo ${i} ${chain_arr[${i}]}
-  echo  curl -X POST --data '{"relay_network_id":"'${chain_arr[${i}]}'","payload":{"data":"{\"jsonrpc\":\"2.0\",\"method\":\"eth_syncing\",\"params\":[],\"id\":1}","method":"POST","path":"","headers":{}}}' http://127.0.0.1:8081/v1/client/sim
-  curl -X POST --data '{"relay_network_id":"'${chain_arr[${i}]}'","payload":{"data":"{\"jsonrpc\":\"2.0\",\"method\":\"eth_syncing\",\"params\":[],\"id\":1}","method":"POST","path":"","headers":{}}}' http://127.0.0.1:8081/v1/client/sim
+  echo curl ${i} ${chain_arr[${i}]}
+  curl -s -X POST --data '{"relay_network_id":"'${chain_arr[${i}]}'","payload":{"data":"{\"jsonrpc\":\"2.0\",\"method\":\"eth_syncing\",\"params\":[],\"id\":1}","method":"POST","path":"","headers":{}}}' http://127.0.0.1:8081/v1/client/sim | awk "{print $2}"
 done
