9c8deb8c28acdb02efed3ae78e4428988bd86053
cventastic <sven@sven-holter.de>
2021-12-16 19:17:03 +0100

    env-variable for snapshot ...

--- bootstrap_skript/pokt_mainnet.sh
+++ bootstrap_skript/pokt_mainnet.sh
@@ -2,7 +2,7 @@
 if [ ! -f /home/app/.pocket/setupdone ]
 then
   mkdir -p /home/app/.pocket/data
-  echo "wget -q -O - $POCKET_SNAPSHOT | tar -xv -C /home/app/.pocket/data/"
+  echo "wget -q -O - '$POCKET_SNAPSHOT' | tar -xv -C /home/app/.pocket/data/"
   wget -q -O - $POCKET_SNAPSHOT | tar -xv -C /home/app/.pocket/data/
   touch /home/app/.pocket/setupdone
 fi
