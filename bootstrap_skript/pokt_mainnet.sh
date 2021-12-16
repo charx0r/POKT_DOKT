db8bd2825d2c573981384f1cd70b3cb51099bc8c
cventastic <sven@sven-holter.de>
2021-12-16 18:20:32 +0100

    put snapshot into variable

--- bootstrap_skript/pokt_mainnet.sh
+++ bootstrap_skript/pokt_mainnet.sh
@@ -2,6 +2,6 @@
 if [ ! -f /home/app/.pocket/setupdone ]
 then
   mkdir -p /home/app/.pocket/data
-  wget -q -O - https://link.us1.storjshare.io/raw/jwb73f5wya2no5fcogrlpmmps7fq/pocket-public-blockchains/pocket-network-data-1214-rc-0.6.3.6.tar | tar -xv -C /home/app/.pocket/data/
+  wget -q -O - $POCKET_SNAPSHOT | tar -xv -C /home/app/.pocket/data/
   touch /home/app/.pocket/setupdone
 fi
