e901b8803020ce80a93d6ba7586d0ac784647145
cventastic <sven@sven-holter.de>
2021-12-28 14:45:41 +0100

    no infile substitution

--- util/prepare.sh
+++ util/prepare.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 source .env
-envsubst < chains/chains_mainnet.json > chains/chains_mainnet.json
-envsubst < chains/chains_testnet.json > chains/chains_testnet.json
+
+originalfile="chains/chains_mainnet.json"
+tmpfile=$(mktemp)
+cp --attributes-only --preserve $originalfile $tmpfile
+cat $originalfile | envsubst > $tmpfile && mv $tmpfile $originalfile
+
+originalfile="chains/chains_testnet.json"
+tmpfile=$(mktemp)
+cp --attributes-only --preserve $originalfile $tmpfile
+cat $originalfile | envsubst > $tmpfile && mv $tmpfile $originalfile
+
 chown -R 1005:1001 chains bootstrap_skript
