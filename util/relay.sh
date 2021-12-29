199f14b3ba19328580d999dacea3d318a73de603
cventastic <sven@sven-holter.de>
2021-12-29 16:39:02 +0100

    homogen ports on stake servers

--- util/relay.sh
+++ util/relay.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+
 # Only geth right now
 declare -A chain_arr
-chain_arr[goerli]="0020"
-chain_arr[rinkeby]="0022"
+chain_arr[goerli]="0026"
+chain_arr[rinkeby]="0025"
 chain_arr[ropsten]="0023"
+chain_arr[mainnet]="0021"
 
 for i in ${!chain_arr[@]}; do
   echo curl ${i} ${chain_arr[${i}]}
