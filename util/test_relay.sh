1abc3cc04e56a0c401d7d83df3f69370b3bd0839
cventastic <sven@sven-holter.de>
2021-12-29 16:47:07 +0100

    added pokt query

--- util/test_relay.sh
+++ util/test_relay.sh
@@ -10,3 +10,6 @@ for i in ${!chain_arr[@]}; do
   echo curl ${i} ${chain_arr[${i}]}
   curl -s -X POST --data '{"relay_network_id":"'${chain_arr[${i}]}'","payload":{"data":"{\"jsonrpc\":\"2.0\",\"method\":\"eth_syncing\",\"params\":[],\"id\":1}","method":"POST","path":"","headers":{}}}' http://127.0.0.1:8081/v1/client/sim | awk "{print $2}"
 done
+
+POKT="0002"
+curl -X POST --data '{"relay_network_id":"$POKT","payload":{"data":"{}","method":"POST","path":"v1/query/height","headers":{}}}' http://localhost:8081/v1/client/sim
