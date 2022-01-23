59cdc5eba2799e848d25a07e54fad20638ac6e5b
cventastic <sven@sven-holter.de>
2022-01-23 21:15:18 +0100

    added pokt/config.json to prepare.sh

--- util/prepare.sh
+++ util/prepare.sh
@@ -15,4 +15,4 @@ tmpfile=$(mktemp)
 cp --attributes-only --preserve $originalfile $tmpfile
 cat $originalfile | envsubst > $tmpfile && mv $tmpfile $originalfile
 
-chown -R 1005:1001 chains bootstrap_skript
+chown -R 1005:1001 chains bootstrap_skript pokt
