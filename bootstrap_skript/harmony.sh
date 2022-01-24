2bd58f4ebb3b92ca5213806ca5867b94557e0606
cventastic <sven@sven-holter.de>
2022-01-24 08:30:11 +0100

    uncomment download in harmony.sh after manual move

--- bootstrap_skript/harmony.sh
+++ bootstrap_skript/harmony.sh
@@ -11,10 +11,10 @@ server_side_encryption = AES256
 storage_class = REDUCED_REDUNDANCY
 EOF
 
-if [ ! -f /setupdone ]
+if [ ! -f /harmony/setupdone ]
 then
   # https://docs.harmony.one/home/network/validators/node-setup/syncing-db#2.-configuring-rclone
   rclone --config=rclone.conf -P -L sync release:pub.harmony.one/mainnet.min/harmony_db_0 /harmony/harmony_db_0 --multi-thread-streams 4 --transfers=8
   rclone --config=rclone.conf -P -L sync release:pub.harmony.one/mainnet.min/harmony_db_1 /harmony/harmony_db_1 --multi-thread-streams 4 --transfers=8
-  touch /setupdone
+  touch /harmony/setupdone
 fi
