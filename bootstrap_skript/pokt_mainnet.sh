2f4879565113615b88e51ce32c58ab14149bf293
czarly <379651+czarly@users.noreply.github.com>
2022-03-16 12:57:37 +0400

    more pokt node fixes

--- bootstrap_skript/pokt_mainnet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ ! -f /home/app/.pocket/setupdone ]
-then
-  mkdir -p /home/app/.pocket/data
-  echo "wget -q -O - '$POCKET_SNAPSHOT' | tar -xv -C /home/app/.pocket/data/"
-  wget -q -O - $POCKET_SNAPSHOT | tar -xv -C /home/app/.pocket/data/
-  touch /home/app/.pocket/setupdone
-fi
-
--- pokt/bootstrap.sh
+++ pokt/bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ ! -f /home/app/.pocket/setupdone ]
+if [ ! -f /home/app/.pocket/data/setupdone ]
 then
   mkdir -p /home/app/.pocket/data
   echo "wget -q -O - '$POCKET_SNAPSHOT' | tar -xv -C /home/app/.pocket/data/"
   wget -q -O - $POCKET_SNAPSHOT | tar -xv -C /home/app/.pocket/data/
-  touch /home/app/.pocket/setupdone
+  touch /home/app/.pocket/data/setupdone
 fi
 
